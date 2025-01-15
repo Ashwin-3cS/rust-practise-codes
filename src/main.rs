486e9fe68fee4459e31a1d23580b217d5ce75441
Ashwin-3cS <flashashwin2004@gmail.com>
2025-01-15 09:36:29 +0530

    added some comment lines

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@
 
 fn main() {
     let x: i32 = 54;
-    println!("This is the value of x {x}!");
+    println!("This is the value of x which is {x}!"); //its more like using placeholder, in C to separate the
+                                                      // variable from the string, we use %d, %s, %f, etc. but in Rust, we use {variable_name}
 }
