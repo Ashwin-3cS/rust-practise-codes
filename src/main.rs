278303c4ba9a5554e7285eb809f897c3adf4b13c
Ashwin-3cS <flashashwin2004@gmail.com>
2025-01-15 09:45:51 +0530

    added some comments about unsigned integer and signed integer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 
 fn main() {
     let x: i32 = 54;
-    println!("This is the value of x which is {x}!"); //its more like using placeholder, in C to separate the
+    println!("This is the value of x which is {x}!"); // its more like using placeholder, in C to separate the
                                                       // variable from the string, we use %d, %s, %f, etc. but in Rust, we use {variable_name}
+                                                      // we are defining the type as i32 which is nothing but signed integer [32 bit in memory is needed in the ram of the machine
+                                                      // int32  defines the signed integer
+                                                      // u32 defines the unsigned integer
 }
