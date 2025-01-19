49930d506b98bf6b891b7e05052897e1e694f6b2
Ashwin-3cS <flashashwin2004@gmail.com>
2025-01-19 18:40:25 +0530

    bool concept in rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+// // fn main() {
+// //     println!("Hello, world!");
+// // }
+
 // fn main() {
-//     println!("Hello, world!");
+//     let x: i32 = 54;
+//     println!("This is the value of x which is {x}"); // its more like using placeholder, in C to separate the
+//                                                      // variable from the string, we use %d, %s, %f, etc. but in Rust, we use {variable_name}
+//                                                      // we are defining the type as i32 which is nothing but signed integer [32 bit in memory is needed in the ram of the machine]
+//                                                      // int32 defines the signed integer
+//                                                      // u32 defines the unsigned integer
+//                                                      // f32 defines the floating point number
 // }
 
 fn main() {
-    let x: i32 = 54;
-    println!("This is the value of x which is {x}"); // its more like using placeholder, in C to separate the
-                                                     // variable from the string, we use %d, %s, %f, etc. but in Rust, we use {variable_name}
-                                                     // we are defining the type as i32 which is nothing but signed integer [32 bit in memory is needed in the ram of the machine
-                                                     // int32 defines the signed integer
-                                                     // u32 defines the unsigned integer
-                                                     // f32 defines the floating point number
+    // let is_male = false;
+    let is_male = true;
+    let is_above_18 = true;
+
+    if is_male {
+        println!("You are a male");
+    } else {
+        println!("You are not a male");
+    }
+
+    if is_male && is_above_18 {
+        print!("You are a legal male adult");
+    }
 }
